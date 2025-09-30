db6e642d61cd6663bfb8fd7d8a53b94fd5833279
Huda Khan <hudakhan@stanford.edu>
2025-09-30 17:29:51 -0600

    updating repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 #lock "~> 3.18.1"
 
 set :application, "dwexp"
-set :repo_url, "git@github.com:sul-dlss-labs/DWExperimentsBlacklight.git"
+set :repo_url, "git@github.com:sul-dlss/DWExperimentsBlacklight.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
