ad80780b33b4a6c8f815c708016b1bc36e82deb0
Nick Budak <budak@stanford.edu>
2025-11-13 16:40:12 -0800

    Handle documents without a provider identifier map
    This can be the case for SDR items harvested from Argo.

--- app/components/dwexp/also_available_component.rb
+++ app/components/dwexp/also_available_component.rb
@@ -1,4 +1,4 @@
-module Dwexp  
+module Dwexp
   class AlsoAvailableComponent < ViewComponent::Base
     def initialize(document:)
       @providers_info = document['provider_identifier_map_struct_ss']
@@ -8,8 +8,9 @@ module Dwexp
     end
 
     def extract_also_available
-      providers = JSON.parse(@providers_info)
-      available_links = providers.map do |provider, id|
+      return if @providers_info.blank?
+
+      JSON.parse(@providers_info).map do |provider, id|
         available_url = provider_url(provider: provider.downcase, id: id)
         # Don't display if no URL mapped for provider OR this is already the landing page URL
         next if available_url.blank? || remove_prefix(url: @url) == remove_prefix(url: available_url)
@@ -31,17 +32,17 @@ module Dwexp
       # since it uses an internal id
       case provider
       when "dryad"
-        "https://datadryad.org/dataset/#{id}" 
+        "https://datadryad.org/dataset/#{id}"
       when "datacite"
         "https://commons.datacite.org/doi.org/#{id}"
       when "zenodo"
         "https://zenodo.org/records/#{id}"
       when "searchworks"
         "https://searchworks.stanford.edu/view/#{id}"
-      else 
+      else
         nil
       end
-    end 
+    end
   end
 
 end
