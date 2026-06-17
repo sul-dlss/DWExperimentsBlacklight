948c95da342317b3ee804012ddbcbf55610b9f43
Cory Lown <corylown@stanford.edu>
2026-06-17 11:24:05 -0400

    Apply designs to show tools

---
+++ app/components/toast_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# A single Bootstrap toast notification. Cloned client-side by the
+# toast-container controller in response to `show-toast` events.
+class ToastComponent < ViewComponent::Base
+end
---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import bootstrap from "bootstrap"
+
+// Connects to data-controller="toast"
+export default class extends Controller {
+  connect() {
+    this.toast = bootstrap.Toast.getOrCreateInstance(this.element)
+
+    this.toast.show()
+  }
+
+  destroy() {
+    this.element.remove()
+  }
+}
